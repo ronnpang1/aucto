0da357ecd15563931aa7ed2b2c84d87a0db1f533
Ronnie <ronn.pang@gmail.com>
2015-06-20 18:46:43 -0400

    test4

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express1' });
-  console.log("dwqdwq");
+  console.log("dwqdwq1");
 });
 
 module.exports = router;
